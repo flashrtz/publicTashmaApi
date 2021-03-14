0f5362fbdca591cc6c41caefcaf5b01bcedd7f18
Iman Dissanayake <imandissanayake12@gmail.com>
2021-03-14 12:06:18 +0530

    commit fix

--- backend/mysql.js
+++ backend/mysql.js
@@ -7,19 +7,19 @@ import mysql from 'mysql';
 //     database        : 'tashmapossystemdb',
 //     insecureAuth : true
 //   });
-  // const mysqlConnection  = mysql.createConnection({
-  //   host            : 'localhost',
-  //   user            : 'admin',
-  //   password        : 'admin',
-  //   database        : 'tashmapossystem',
-  //   insecureAuth : true
-  // });
-
-   const mysqlConnection  = mysql.createConnection({
-    host            : '45.77.42.114',
+  const mysqlConnection  = mysql.createConnection({
+    host            : 'localhost',
     user            : 'admin',
     password        : 'admin',
     database        : 'tashmapossystem',
     insecureAuth : true
   });
+
+  //  const mysqlConnection  = mysql.createConnection({
+  //   host            : '45.77.42.114',
+  //   user            : 'admin',
+  //   password        : 'admin',
+  //   database        : 'tashmapossystem',
+  //   insecureAuth : true
+  // });
 export default mysqlConnection;
